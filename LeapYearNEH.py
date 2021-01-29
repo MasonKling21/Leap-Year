5a433e3be544bffc61b7ce3c981704a37b608098
Mason Kling <klingma@oregonstate.edu>
2021-01-28 19:19:11 -0800

    Added leap year w/ error handling and leap year w/o error handling

---
+++ LeapYearNEH.py
@@ -0,0 +1,11 @@
+# To run enter "python3 Mason_Kling_hw1.py" in terminal
+# Or however your computer runs python files
+
+num = input()
+if(int(num) % 4 == 0):
+    if(int(num) % 400 == 0 or int(num) % 100 != 0):
+        print(num, " is a leap year")
+    else:
+        print(num, " is not a leap year")
+else:
+    print(num, " is not a leap year")
---
+++ LeapYearWEH.py
@@ -0,0 +1,18 @@
+# To run enter "python3 Mason_Kling_hw1.py" in terminal
+# Or however your computer runs python files
+
+while True:
+    try:
+        num = int(input())
+        break
+    except:
+        print("Error! Invalid input.")
+
+
+if(num % 4 == 0):
+    if(int(num) % 400 == 0 or num % 100 != 0):
+        print(num, " is a leap year")
+    else:
+        print(num, " is not a leap year")
+else:
+    print(num, " is not a leap year")
